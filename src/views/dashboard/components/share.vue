339a6a74fd45d3b148e281cbefc9cd3e750062e7
4smil3 <939911658@qq.com>
2026-04-01 09:49:49 +0800

    1

--- src/views/dashboard/components/share.vue
+++ src/views/dashboard/components/share.vue
@@ -8,11 +8,11 @@
         <img src="@/assets/images/share/qrcode.png" v-if="isShare" alt="二维码" class="qrcode">
       </div>
       <div class="shareBtnBox" v-if="!isShare">
-        <img src="@/assets/images/share/want_share.png" @click="generateImg" alt="我要分享" />
+        <img src="@/assets/images/share/want_share.png" @click.stop="generateImg" alt="我要分享" />
       </div>
       <div class="shareBtnBox created" v-else>
-        <img @click="saveImg" src="@/assets/images/share/save.png" alt="保存图片" />
-        <img src="@/assets/images/share/share.png" alt="分享" />
+        <img @click.stop="saveImg" src="@/assets/images/share/save.png" alt="保存图片" />
+        <img @click.stop="openBilibiliShare"  src="@/assets/images/share/share.png" alt="分享" />
       </div>
     </div>
   </van-popup>
@@ -30,7 +30,9 @@ export default {
       robotImg: "", //机甲图片
       isShare: false, //是否分享
       base64Img: "", //分享图片base64
+      image_url: "", // 分享图片url
       nickname: "", //用户昵称
+
     };
   },
   watch: {},
@@ -135,6 +137,112 @@ export default {
       document.body.removeChild(link);
       Toast.success("保存成功");
       this.closeShare();
+    },
+    //打开分享
+    async openBilibiliShare() {
+      const res = await this.uploadImage();
+      //调用分享方法
+      this.share(res.image_url);
+      this.closeShare();
+    },
+    //实际分享逻辑
+    share(image_url) {
+      console.log("share", image_url);
+      const { inBiliApp, isSupport, callNative } = window.biliBridge;
+      if (inBiliApp) {
+        isSupport("share.showShareMpcWindow").then((support) => {
+          if (support) {
+            callNative({
+              method: "share.showShareMpcWindow",
+              data: {
+                onShareCallbackId: "123",
+                // 自定义分享渠道和顺序
+                // 支持渠道参数：['generic'，'dynamic'，'weixin'，'weixin_monment'，'qq'，'sina'，'q_zone'，'copy']
+                shareChannelQueue: [
+                  "weixin",
+                  "q_zone",
+                  "weixin_monment",
+                  "dynamic",
+                ],
+                generic: {
+                  type: "image", // (必需，包含'text', 'image')
+                  title: "分享标题", // (必需)
+                  imageUrl: "图片链接", // (必需)
+                  text: "描述一下", // (必需)
+                },
+                dynamic: {
+                  title: "分享标题", // (必需)
+                  content_type: 2048, // (必需)
+                  cover_url: "封面图", // (必需)
+                  description: "描述描述描述描述描述描述描述", // (必需)
+                  images: ["", "", ""], // (非必需)
+                  publish: false, // (非必需)
+                  edit_content: "动态预设文案", // (非必需)
+                  repost_code: 123, // (非必需)
+                  images_online: "", // (非必需)
+                },
+                weixin: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web', 'min_program')
+                  title: "分享标题", // (必需)
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需) 如果是分享当前页面，可使用 window.location.href (注意是否带 query 参数)，或找站内对接人要到页面部署的线上地址
+                  imageUrl: image_url, // (type为image类型必需)
+                  program_id: "123", // (type为min_program类型必需，该id是小程序的原始id, 类似于gh_xxxxxxxx)
+                  program_path: "path", // (type为min_program类型必需)
+                  media_src: "多媒体地址", //(type为video audio类型必需)
+                },
+                weixin_monment: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web')
+                  title: "分享标题", // (必需)
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需)
+                  imageUrl: image_url, // (type为image类型必需)
+                  media_src: "多媒体地址", // (type为video audio类型必需)
+                },
+                qq: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web')
+                  title: "分享标题", // (必需)
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需)
+                  imageUrl: image_url, // (type为image类型必需)
+                  media_src: "多媒体地址", // (type为video audio类型必需)
+                },
+                sina: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web')
+                  title: "分享标题", // (必需, 新版新浪客户端不会识别该参数, 但是分享组件里需要识别该参数，业务方可以传自己title或传'哔哩哔哩 (゜-゜)つロ 干杯~')
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需)
+                  imageUrl: image_url, // (type为image类型必需)
+                  media_src: "多媒体地址", // (type为video audio类型必需)
+                },
+                q_zone: {
+                  type: "image", // (必需, 包含'text', 'image', 'web')
+                  title: "分享标题", // (必需)
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需)
+                  imageUrl: image_url, // (type为image类型必需)
+                },
+                copy: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web', copy渠道可以填写'text')
+                  title: "分享标题", // (必需)
+                  url: "", // (必需)
+                },
+                default: {
+                  type: "image", // (必需, 包含'text', 'image', 'video', 'audio', 'web')
+                  title: "分享标题", // (必需)
+                  text: "描述一下", // (必需)
+                  url: "分享链接", // (必需)
+                  imageUrl: image_url, // (type为image类型必需)
+                  media_src: "多媒体地址", // (type为video audio类型必需)
+                },
+              },
+              callback: () => {},
+            });
+          } else {
+            // not support
+          }
+        });
+      }
     },
   },
 };
