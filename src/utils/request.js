94752457efb353c66cc6fe4bb0fc6489d56b02cc
4smil3 <939911658@qq.com>
2026-04-17 20:24:57 +0800

    没登录判断

--- src/utils/request.js
+++ src/utils/request.js
@@ -43,8 +43,8 @@ request.interceptors.response.use(
       return Promise.resolve(res.result);
     } else {
       // 处理错误情况
-      Toast.error(res.error_msg);
-      return Promise.reject(new Error(res.error_msg || "请求失败"));
+      // Toast.error(res.error_msg);
+      return Promise.reject(res);
     }
   },
   (error) => {
